6889472d6bde080610360b828e0e1350910f32fe
Code Inverness <hello@rfraser.co.uk>
2023-07-23 22:36:48 +0100

    Update page.tsx
    added a working function to the clear button

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,16 @@ export default function Home() {
   }
 
   function changeColor(e) {
-    setColor('bg-black');
+    const classes = e.target.className.split(' ');
+    const colorClass = classes[classes.length - 1];
+    setColor(colorClass);
+  }
+
+  function clearGrid(e) {
+    const cells = document.querySelectorAll('.flex-1');
+    cells.forEach(cell => {
+      cell.className = `flex-1 h-8 w-8 md:h-12 md:w-12 lg:h-16 lg:w-16 bg-zinc-100`;
+    })
   }
 
   return (
@@ -32,15 +41,17 @@ export default function Home() {
         ))}
         <div className='flex justify-between shadow-md bg-red-500 text-white items-center font-extrabold p-8'>
           <div className='flex space-x-2'>
-            <button className='w-8 h-8 bg-black rounded-full' onClick={changeColor}></button>
-            <div className='w-8 h-8 bg-white rounded-full'></div>
-            <div className='w-8 h-8 bg-red-600 rounded-full'></div>
-            <div className='w-8 h-8 bg-yellow-500 rounded-full'></div>
-            <div className='w-8 h-8 bg-gradient-conic from-green-400 to-purple-500 rounded-full'></div>
+            <button className='w-8 h-8 rounded-full bg-black' onClick={changeColor}></button>
+            <button className='w-8 h-8 rounded-full bg-white' onClick={changeColor}></button>
+            <button className='w-8 h-8 border-red-700 border-2 rounded-full bg-red-500' onClick={changeColor}></button>
+            <button className='w-8 h-8 rounded-full bg-yellow-400' onClick={changeColor}></button>
+            <button className='w-8 h-8 rounded-full bg-blue-500' onClick={changeColor}></button>
+            {/* make a random colour button */}
+            {/* <button className='w-8 h-8 rounded-full' onClick={changeColor}></button> */}
           </div>
           <h1 className='text-4xl'>Etch a Sketch</h1>
           <div className='space-x-4'>
-            <button className='bg-zinc-900 p-2 rounded-md'>Clear</button>
+            <button className='bg-zinc-900 p-2 rounded-md' onClick={clearGrid}>Clear</button>
             <button className='bg-zinc-900 p-2 rounded-md'>Grid Size</button>
           </div>
         </div>
