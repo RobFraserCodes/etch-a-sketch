1ccfe556980a877212153ac297db31171930eab9
Code Inverness <hello@rfraser.co.uk>
2023-07-23 22:53:29 +0100

    Clear Button
    added a cool shake animation when you clear the etch a sketch!

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ const gridSize = 16;
 export default function Home() {
   const grid = Array.from({ length: gridSize }, (_, i) => i + 1);
   const [color, setColor] = useState('bg-red-500');
+  const [shake, setShake] = useState(false);
 
   const handleMouseOver = (e) => {
     e.target.className = `flex-1 h-8 w-8 md:h-12 md:w-12 lg:h-16 lg:w-16 ${color}`;
@@ -22,12 +23,14 @@ export default function Home() {
     const cells = document.querySelectorAll('.flex-1');
     cells.forEach(cell => {
       cell.className = `flex-1 h-8 w-8 md:h-12 md:w-12 lg:h-16 lg:w-16 bg-zinc-100`;
+      setShake(true);
+      setTimeout(() => setShake(false), 600);
     })
   }
 
   return (
-    <div className='flex flex-col justify-center items-center space-y-4'>
-      <div className='max-w-4xl bg-zinc-100 border-red-500 border-8 rounded-md m-8'>
+    <div className='flex flex-col justify-center items-center'>
+      <div className={`max-w-4xl bg-zinc-600 m-8 border-8 border-red-500 rounded-lg ${shake ? 'animate-shake' : ''}`}>
         {grid.map((_, rowIndex) => (
           <div key={rowIndex} className='flex justify-center text-center items-center'>
             {grid.map((_, cellIndex) => (
@@ -39,7 +42,7 @@ export default function Home() {
             ))}
           </div>
         ))}
-        <div className='flex justify-between shadow-md bg-red-500 text-white items-center font-extrabold p-8'>
+        <div className='flex justify-between shadow-md bg-red-500 items-center p-8'>
           <div className='flex space-x-2'>
             <button className='w-8 h-8 rounded-full bg-black' onClick={changeColor}></button>
             <button className='w-8 h-8 rounded-full bg-white' onClick={changeColor}></button>
@@ -49,10 +52,10 @@ export default function Home() {
             {/* make a random colour button */}
             {/* <button className='w-8 h-8 rounded-full' onClick={changeColor}></button> */}
           </div>
-          <h1 className='text-4xl'>Etch a Sketch</h1>
+          <h1 className='text-4xl text-center text-red-600 font-black'>Etch a Sketch</h1>
           <div className='space-x-4'>
-            <button className='bg-zinc-900 p-2 rounded-md' onClick={clearGrid}>Clear</button>
-            <button className='bg-zinc-900 p-2 rounded-md'>Grid Size</button>
+            <button className='bg-zinc-100 p-2 w-16 h-16 rounded-full text-center' onClick={clearGrid}>Clear</button>
+            <button className='bg-zinc-100 p-2 rounded-md'>Grid Size</button>
           </div>
         </div>
       </div>
